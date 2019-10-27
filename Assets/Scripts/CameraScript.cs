ac60921819104e04edd4a3444b65db7a5cfe0765
arnaupool <arnyr@hotmail.es>
2019-10-27 12:43:50 +0100

    Mejoras insignificantes en el script de PlayerController

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -9,7 +9,7 @@ public class CameraScript : MonoBehaviour
 	public float smoothSpeed = 0.125f;
 	public Vector3 offset;
 
-	void FixedUpdate ()
+	void LateUpdate ()
 	{
 		Vector3 desiredPosition = target.position + offset;
 		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
