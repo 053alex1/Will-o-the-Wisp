b5a6d83658a84ca8170824f94f8928df349f03cd
arnaupool <arnyr@hotmail.es>
2019-10-29 15:03:35 +0100

    Arreglando lo que he roto

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameManager instance = null;
+    public GameObject playerObject = null;
+    public PlayerController playerController;
+    void Awake()
+    {
+        singleton();
+        getPlayer();
+    }
+
+    void singleton() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != null) Destroy(gameObject);
+    }
+    public GameObject getPlayer()
+    {
+        if (playerObject == null)
+            playerObject = GameObject.FindGameObjectWithTag("Dagda");
+
+        return playerObject;
+    }
+    public PlayerController getPlayerController()
+    {
+        if (playerController == null)
+            playerController = getPlayer().GetComponent<PlayerController>();
+
+        return playerController;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Transform tr;
+    GameObject player;
+    public float speed = 50f;
+    public Vector3 move = new Vector3(0, 55, -55);
+    public float jumpHeight = 2f;
+    private bool isGrounded;
+    public GameObject bubble;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Dagda");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Let\'s get this bread.");
+        tr = player.transform.GetComponent<Transform>();
+        rb = player.GetComponent<Rigidbody>();
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        createBubble();
+    }
+
+    void Move()
+    {
+        move = new Vector3(Input.GetAxis("Horizontal") * Time.deltaTime * speed, rb.velocity.y, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        tr.Translate(move);
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity += -1f * Physics.gravity.normalized * this.CalculateJumpVerticalSpeed(this.jumpHeight);
+            isGrounded = false;
+        }
+    }
+
+    float CalculateJumpVerticalSpeed(float height)
+    {
+        return Mathf.Sqrt(2f * height * Physics.gravity.magnitude);
+    }
+
+    void createBubble()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            Debug.Log("Bubble instantiated");
+            Instantiate(bubble, getBubblePosition(), Quaternion.identity);
+        }
+    }
+
+    Vector3 getPlayerPosition()
+    {
+        return player.transform.position;
+    }
+
+    Vector3 getBubblePosition()
+    {
+        return getPlayerPosition() + new Vector3(0, 15, 0); //Falta por determinar la altura de Dagda
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Ground") isGrounded = true;
+    }
+}
