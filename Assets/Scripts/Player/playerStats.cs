fc5703e3f5dc099c59ada1719e39348c20015d8c
arnaupool <arnyr@hotmail.es>
2019-11-05 13:43:30 +0100

    Sistema de maná v0

--- Assets/Scripts/Player/playerStats.cs
+++ Assets/Scripts/Player/playerStats.cs
@@ -4,10 +4,45 @@ using UnityEngine;
 
 public class playerStats : MonoBehaviour
 {
-    public float lightDamage = 2f;
-    public float heavyDamage = 5f;
+    public float hp = 0f;
+    public float mana = 20f;
     public float speed = 50f;
     public float jumpHeight = 0.5f;
+    public float lightDamage = 2f;
+    public float heavyDamage = 5f;
     public bool isGrounded;
     public bool isRunning;
+    private float maxHealth = 100f;
+    private float manaRegenPerSec = 0.5f;
+    private float delay = 3f;   //Tiempo entre el último ataque de maná y la regeneración de este
+    private float maxMana = 20f;
+    private float timestamp = 0f;
+    
+    public float reduceHp(float amount) {
+        return hp -= amount;
+    }
+
+    public void fillHp() {
+        hp = maxHealth;
+    }
+
+    public void reduceMana(float amount) {
+        timestamp = Time.time;              // Cuando se 
+        mana = Mathf.Max(0, mana - amount);
+    }
+
+    public void regenMana() {
+        if (Time.time > (timestamp + delay)) {      // Sólo se recupera el maná cuando hayan pasado los segundos del delay
+            mana = Mathf.Min(maxMana, mana + (manaRegenPerSec * Time.deltaTime)); // (* Time.deltaTime) ?
+            Debug.Log("Mana is " + mana);
+        }
+    }
+
+    void Update() {
+        regenMana();
+    }
+    void Start() {
+        InvokeRepeating("regenMana", 0f, manaRegenPerSec);  // Con esta función se invoca a regenMana() cada 0.5 segundos
+    }
+
 }
