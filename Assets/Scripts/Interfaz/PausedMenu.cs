9ea4f6fc789eb3af8e5be4838f6270d1e7d06c74
HaruKawai <angelginer27@gmail.com>
2019-11-14 16:06:16 +0100

    camera interna

---
+++ Assets/Scripts/Interfaz/PausedMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausedMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public GameObject canvas;
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+        if(isPaused)
+        {
+            ActivarMenu();
+        }
+        else
+        {
+            DesactivarMenu();
+        }
+    }
+    public void ActivarMenu() 
+    {
+        GameObject.Find("Camera").GetComponent<CameraFollow>().enabled = false;
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+        canvas.SetActive(true);
+        pauseMenu.SetActive(true);
+    }
+    public void DesactivarMenu()
+    {
+        GameObject.Find("Camera").GetComponent<CameraFollow>().enabled = true;
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        canvas.SetActive(false);
+        pauseMenu.SetActive(false);
+    }
+}
