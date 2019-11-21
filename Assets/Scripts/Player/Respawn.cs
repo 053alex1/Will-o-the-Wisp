eb6706a2cd5015b4830fd64889b64b940cf5cfbf
arnaupool <arnyr@hotmail.es>
2019-11-21 13:49:51 +0100

    Sistema de respawn hecho

---
+++ Assets/Scripts/Player/Respawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    private GameObject player;
+    private GameObject spawnPoint;
+    void Awake() {
+        player = GameObject.FindGameObjectWithTag("Dagda");
+        spawnPoint = GameObject.FindGameObjectWithTag("spawnPoint");
+    }
+
+
+    void OnTriggerEnter(Collider col) {
+        Debug.Log("Something collided");
+        player.transform.position = spawnPoint.transform.position;
+        //Instanciar un efecto de reaparición o algo
+    }
+}
