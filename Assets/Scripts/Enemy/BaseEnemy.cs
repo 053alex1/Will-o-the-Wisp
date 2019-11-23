74a6bd37050b73992a27639b1c0fca0031c8eacb
did <diegorospagan@gmail.com>
2019-11-23 22:52:23 +0100

    vida

--- Assets/Scripts/Enemy/BaseEnemy.cs
+++ Assets/Scripts/Enemy/BaseEnemy.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 public class BaseEnemy : MonoBehaviour
 {
     public float hp = 10f;
+    private Transform target;
+    GameObject canvas;
+
+    GameObject x;
+    // Use this for initialization
+    void Start() {
+       target = Camera.main.transform;
+       canvas = transform.Find("Canvas").gameObject;
+    }
+   
+    // Update is called once per frame
+    void Update() {
+        Vector3 v = target.position - transform.position;
+        v.Normalize();
+        canvas.transform.rotation = Quaternion.LookRotation(new Vector3(v.x,0,v.z));
+    }
+   
+
 
     public void getHit(float damage) {
         hp -= damage;
