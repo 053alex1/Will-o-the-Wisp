4dd5bb94df4039c274df8a259f8a1c167aaf4f38
HaruKawai <angelginer27@gmail.com>
2019-11-27 21:45:03 +0100

    Revert "sonido,dialogo"
    This reverts commit 937e881f2ae91a73736b1dea448f30b0c3deeabf.

--- Assets/Scripts/Interfaz/Brightness.cs
+++ Assets/Scripts/Interfaz/Brightness.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class Brightness : MonoBehaviour
 {
 
-    //float rbgValue = 0.5f;
+    float rbgValue = 0.5f;
     // Start is called before the first frame update
     /// <summary>
     /// OnGUI is called for rendering and handling GUI events.
     /// This function can be called multiple times per frame (one call per event).
     /// </summary>
-    public void Setbrillo (float brillo) {
-        RenderSettings.ambientIntensity = brillo;
-        Debug.Log(brillo);
+    void OnGUI()
+    {
+        //rbgValue = GUI.HorizontalSlider (new Rect (Screen.vidth/2 -50, 90, 200, 30), rbgValue, 0f, 1.0f);
+        RenderSettings.ambientLight = new Color (rbgValue,rbgValue,rbgValue,1);
     }
 }
