4b766c585834c2f75445f470d54885cd0c9b9fc1
Alejandro Alonso Rodríguez <053alex1@gmail.com>
2019-11-27 16:52:16 +0100

    GUI casi funcional

---
+++ Assets/Scripts/Player/GUIInteraction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIInteraction : MonoBehaviour
+{
+    public Image healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //healthBar = GameObject.FindGameObjectWithTag("life");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeLife(float health)
+    {
+        Debug.Log("2");
+        healthBar.fillAmount = health / 100f;
+    }
+}
