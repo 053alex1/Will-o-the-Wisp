67c69474c3c413bdd5fe192f29693c6827351582
mirandaguerriero <36904406+mirandaguerriero@users.noreply.github.com>
2019-11-27 16:51:57 +0100

    fuck music

--- Assets/Resources/Sound.cs
+++ Assets/Resources/Sound.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Sounds { JUMP, HIT }
-public class Sound : MonoBehaviour
-{
-    public AudioSource[] sonidos; // Use this for initialization
-    void Update()
-    {
+public enum Sounds {JUMP, HIT }
 
+public class Sound : MonoBehaviour
+
+{
+    public AudioSource[] Audios; // Use this for initialization
+   
+    void Start()
+    {
+        Audios = GetComponents<AudioSource>();
+    }
+
+  /*/  void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Space)) //Deteccion de si es pulsada la barra espaciadora
             if (sonidos[(int)Sounds.JUMP].isPlaying) //Si el audio se esta reproduciendo
                 sonidos[(int)Sounds.JUMP].Pause(); //Pausar la reproduccion
             else
                 sonidos[(int)Sounds.JUMP].Play(); //Reproducir audio
-    }
+    }*/
 }
