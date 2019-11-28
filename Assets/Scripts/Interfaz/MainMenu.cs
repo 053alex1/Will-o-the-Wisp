c45c2ecc2993d341101d55a123e532e110579558
HaruKawai <angelginer27@gmail.com>
2019-11-28 12:41:38 +0100

    menu

--- Assets/Scripts/Interfaz/MainMenu.cs
+++ Assets/Scripts/Interfaz/MainMenu.cs
@@ -6,13 +6,13 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
    public void PlayGame1() {
-       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       SceneManager.LoadScene(1);
    }
    public void MenuGame() {
-       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+       SceneManager.LoadScene(0);
    }
    public void PlaySameGame() {
-       
+       Debug.Log("ReintentarNivell");
        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
    }
 
